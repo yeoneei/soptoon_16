437e09154fe1e3bd0fa45d51baaca443514dbc4d
yeoneei <soso3786@gmail.com>
2019-05-29 14:54:47 +0900

    [update] project setting

---
+++ module/responseMessage.js
@@ -0,0 +1,29 @@
+module.exports = {
+    NULL_VALUE: "필요한 값이 없습니다",
+    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+    ID_OR_PW_NULL_VALUE: "아이디/비밀번호 값이 없습니다.",
+    CREATED_USER: "회원 가입 성공",
+    DELETE_USER: "회원 탈퇴 성공",
+
+    ALREADY_USER: "이미 회원입니다.",
+    NO_USER: "존재하지 않는 회원입니다.",
+    MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+    LOGIN_SUCCESS: "로그인 성공",
+    LOGIN_FAIL: "로그인 실패",
+
+    SAVE_SUCCESS: "데이터 저장 성공",
+    SAVE_FAIL: "데이터 저장 실패",
+    READ_FAIL: "데이터 읽기 실패",
+    READ_SUCCESS : "데이터 읽기 성공",
+
+    STUDENT_SELECT_SUCCESS: "학생 조회 성공",
+    NO_STUDENT: "존재하지 않는 학생입니다",
+    STUDENT_SELECT_FAIL: "학생 조회 실패",
+
+    POST_SLECET_FAILE:"같은 제목이 존재 합니다.",
+    POST_SAVE_FAILE:"게시글 저장 실패",
+    POST_THUMB_SAVE_SUCCESS : "썸네일 저장 성공",
+    POST_CONTENTS_SAVE_SUCCESS :"게시글 저장 성공",
+    POST_READ_FAILE:"게시글 읽어오기 실패",
+    POST_READ_SUCCESS : "게시글 읽어오기 성공",
+};
